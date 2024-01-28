c6004b301040117781d58c0834ffa4eb7418d532
Pranjal826 <pranjalshukla245@gmail.com>
2024-01-29 01:02:59 +0530

    My

--- models/config.js
+++ models/config.js
@@ -1,8 +1,14 @@
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017/makeup')
+mongoose.connect(
+    'mongodb+srv://pranjalshukla245:RgiDeemJ5JkE7DGt@cluster0.vtjn0jv.mongodb.net/Makeup-Site?retryWrites=true&w=majority',
+    {
+      useUnifiedTopology: true,
+    }
+  )
 .then(()=>{
-    console.log('connected')
-})
+    console.log('connected to db')
+}
+)
 .catch((err)=>{
     console.log(err)
 })
